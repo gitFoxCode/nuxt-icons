fa0b7ee0c532b85c5cb82ccd80efecb1fbec1e65
penkhasoveg <pen.egor2002@gmail.com>
2022-11-24 17:02:19 +0300

    feat(playground): reactive update example

--- playground/app.vue
+++ playground/app.vue
@@ -5,14 +5,34 @@
       <li class="colored">
         Icon with color from text: <nuxt-icon name="fire" />
       </li>
-      <li>Icon with defs: <nuxt-icon name="css3" filled /> <nuxt-icon name="javascript" filled /></li>
-      <li>Subfolder icons: <nuxt-icon name="user/badge" /> User, <nuxt-icon name="admin/badge" /> Admin, <nuxt-icon name="admin/form/bug" /></li>
+      <li>
+        Icon with defs: <nuxt-icon name="css3" filled />
+        <nuxt-icon name="javascript" filled />
+      </li>
+      <li>
+        Subfolder icons: <nuxt-icon name="user/badge" /> User,
+        <nuxt-icon name="admin/badge" /> Admin,
+        <nuxt-icon name="admin/form/bug" />
+      </li>
+      <li>
+        Reactive change:
+        <nuxt-icon :name="icon ? 'user/badge' : 'admin/form/bug'" />
+        <button @click="icon = !icon">
+          Update
+        </button>
+      </li>
     </ul>
   </div>
 </template>
 
+<script setup lang="ts">
+import { ref } from '#imports'
+
+const icon = ref(false)
+</script>
+
 <style>
-.colored{
+.colored {
   color: orange;
 }
 </style>
