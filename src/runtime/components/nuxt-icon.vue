e8c525c7a597e2e713a3a13f8385382446c6bf19
penkhasoveg <pen.egor2002@gmail.com>
2022-11-24 17:01:59 +0300

    fix(nuxt-icon): update icon on change props

--- src/runtime/components/nuxt-icon.vue
+++ src/runtime/components/nuxt-icon.vue
@@ -1,9 +1,13 @@
 <template>
-  <span class="nuxt-icon" :class="{ 'nuxt-icon--fill': !filled }" v-html="icon" />
+  <span
+    class="nuxt-icon"
+    :class="{ 'nuxt-icon--fill': !filled }"
+    v-html="icon"
+  />
 </template>
 
 <script setup lang="ts">
-import { ref } from '#imports'
+import { ref, watchEffect } from '#imports'
 
 const props = defineProps({
   name: String,
@@ -15,24 +19,31 @@ const props = defineProps({
 })
 const icon = ref('')
 
-try {
-  const iconsImport = import.meta.glob('assets/icons/**/**.svg', { as: 'raw', eager: false })
-  const rawIcon = await iconsImport[`/assets/icons/${props.name}.svg`]()
-  icon.value = rawIcon
-} catch {
-  console.error(`[nuxt-icons] Icon '${props.name}' doesn't exist in 'assets/icons'`)
-}
-
+watchEffect(async () => {
+  try {
+    const iconsImport = import.meta.glob('assets/icons/**/**.svg', {
+      as: 'raw',
+      eager: false
+    })
+    const rawIcon = await iconsImport[`/assets/icons/${props.name}.svg`]()
+    icon.value = rawIcon
+  } catch {
+    console.error(
+      `[nuxt-icons] Icon '${props.name}' doesn't exist in 'assets/icons'`
+    )
+  }
+})
 </script>
 
 <style>
-.nuxt-icon svg{
-    width: 1em;
-    height: 1em;
-    margin-bottom: 0.125em;
-    vertical-align: middle;
+.nuxt-icon svg {
+  width: 1em;
+  height: 1em;
+  margin-bottom: 0.125em;
+  vertical-align: middle;
 }
-.nuxt-icon.nuxt-icon--fill, .nuxt-icon.nuxt-icon--fill * {
-    fill: currentColor !important;
+.nuxt-icon.nuxt-icon--fill,
+.nuxt-icon.nuxt-icon--fill * {
+  fill: currentColor !important;
 }
 </style>
