754e5769bf85e373721a3ba7dba446fd213205e4
Ted de Koning <1109781+tdekoning@users.noreply.github.com>
2023-03-14 16:40:46 +0100

    chore: make typing of icon ref match the rest of the code.

--- src/runtime/components/nuxt-icon.vue
+++ src/runtime/components/nuxt-icon.vue
@@ -14,7 +14,7 @@ const props = withDefaults(defineProps<{
   filled?: boolean
 }>(), { filled: false })
 
-const icon = ref('')
+const icon = ref<string | Record<string, any>>('')
 
 watchEffect(async () => {
   try {
